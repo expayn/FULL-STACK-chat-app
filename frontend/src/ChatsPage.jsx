ea130760ac9e5ea8cda072d0211da1f13d632e16
expayn <ascodev25@gmail.com>
2024-04-25 19:39:00 +0300

    3

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,15 @@
-const ChatsPage = () => {
-	return <div className='background'>chats...</div>
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = props => {
+	return (
+		<div className='background'>
+			<PrettyChatWindow
+				projectId='76bd7a40-1ed3-45c1-a215-07b80b8fdc18'
+				username={props.user.username}
+				secret={props.user.secret}
+			/>
+		</div>
+	)
 }
+
 export default ChatsPage

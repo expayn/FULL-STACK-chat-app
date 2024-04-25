c80797b6237402c7e257650686f6b82bf949c502
expayn <ascodev25@gmail.com>
2024-04-25 19:16:43 +0300

    2

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+app.post('/authenticate', async (req, res) => {
+    const { username } = req.body;
+    try {
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username: username, secret: username, first_name: username },
+            { headers: { "private-key": "84a7927f-cb82-4bc6-bdfe-63a2498039dc" } }
+        );
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
 });
 
-app.listen(3001);
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
